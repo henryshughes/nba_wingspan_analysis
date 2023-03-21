c0f7155fcbeb4a70da46abb20e53aea50984c733
Henry Hughes <hockeyfan534@gmail.com>
2023-03-21 17:25:41 -0500

    Add function to convert lists

--- nbadata.py
+++ nbadata.py
@@ -34,42 +34,32 @@ for row in rows:
     wingspan = row.find_all('td')[4].text
     wingspan_list_string.append(wingspan)
 
-#Loop to turn the list item from strings into floating point values
-for i in range(len(height_list_string)):
+#Function to convert the lists to floating point values from strings
+def convert_list(input_list):
+    output_list = []
+    for i in range(len(input_list)):
       # Split the string into feet and inches components
 
-    feet_str, inches_str = height_list_string[i].split("\'")
+        feet_str, inches_str = input_list[i].split("\'")
 
-    # Convert the feet component to inches
-    feet_inches = float(feet_str) * 12
+        # Convert the feet component to inches
+        feet_inches = float(feet_str) * 12
 
-    # Convert the inches component to a float
-    inches = float(inches_str[:-1])
+        # Convert the inches component to a float
+        inches = float(inches_str[:-1])
 
-    # Add the feet and inches components together to get the total height in inches
-    total_inches = feet_inches + inches
-    total_feet = total_inches/12
+        # Add the feet and inches components together to get the total height in inches
+        total_inches = feet_inches + inches
+        total_feet = total_inches/12
 
-    height_list_int.append(round(total_feet, 2))
+        output_list.append(round(total_feet, 2))
 
-#Loop to turn the list item from strings into floating point values
-for i in range(len(wingspan_list_string)):
-    # Split the string into feet and inches components
+    return output_list
 
-    feet_str, inches_str = wingspan_list_string[i].split("\'")
+#Convert lists
+height_list_int = convert_list(height_list_string)
+wingspan_list_int = convert_list(wingspan_list_string)
 
-    # Convert the feet component to inches
-    feet_inches = float(feet_str) * 12
-
-    # Convert the inches component to a float
-    inches = float(inches_str[:-1])
-
-    # Add the feet and inches components together to get the total height in inches
-    total_inches = feet_inches + inches
-    total_feet = total_inches/12
-
-    wingspan_list_int.append(round(total_feet, 2))
-    
 #Plot using matplotlib
 plt.plot(height_list_int, wingspan_list_int, 'ko', markersize = 1)
 plt.plot([5.8, 8], [5.8, 8], linewidth=2)
